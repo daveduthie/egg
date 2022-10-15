2ea66159463dee491cfdbf24cba3158b9ac7a86d
David Duthie <dave.a.duthie@gmail.com>
2022-10-15 15:33:46 +0200

    add some tests for bitmap utils

--- crates/egg-index/src/lib.rs
+++ crates/egg-index/src/lib.rs
@@ -1,12 +1,20 @@
-#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone)]
-struct Node<T> {
-    data: Vec<T>,
+// https://eugene-babichenko.github.io/blog/2019/11/13/rust-popcount-intrinsics/
+#[cfg_attr(target_arch = "x86_64", target_feature(enable = "popcnt"))]
+pub fn popcnt(n: u64) -> u32 {
+    n.count_ones()
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone)]
-pub struct Tree<T> {
-    root: Node<T>,
-    tail: Option<Vec<T>>,
+const ALPHABET_SIZE: u64 = 64;
+
+pub fn is_set(bitmap: u64, n: u64) -> bool {
+    let shift = ALPHABET_SIZE - n - 1;
+    let mask = 1 << shift; // Shift 0b1 n places to the left
+    bitmap & mask != 0
+}
+
+pub fn bitmap_index_of(bitmap: u64, key: u64) -> u32 {
+    let shift = ALPHABET_SIZE - key;
+    popcnt(bitmap >> shift)
 }
 
 #[cfg(test)]
@@ -14,17 +22,40 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let data = vec![String::from("Hello"), String::from("Egg")];
-        let root = Node { data };
-        let expected = Tree {
-            root: root.clone(),
-            tail: None,
-        };
-        let actual = Tree {
-            root,
-            tail: Some(Vec::new()),
-        };
-        assert_eq!(expected, actual);
+    fn is_set_test() {
+        let bitmap = 0b10100000 << (ALPHABET_SIZE - 8);
+        assert_eq!(true, is_set(bitmap, 0));
+        assert_eq!(false, is_set(bitmap, 1));
+        assert_eq!(true, is_set(bitmap, 2));
+        assert_eq!(false, is_set(bitmap, 3));
+    }
+
+    #[test]
+    fn bitmap_index_of_test() {
+	let bitmap = 0b01011000 << (ALPHABET_SIZE - 8);
+        assert_eq!(0, bitmap_index_of(bitmap, 1));
+        assert_eq!(1, bitmap_index_of(bitmap, 3));
+        assert_eq!(2, bitmap_index_of(bitmap, 4));
     }
 }
+
+// #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+// struct Tree {
+//     bitmap: u64,
+//     children: Vec<Node>,
+// }
+
+// #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+// struct Leaf {}
+
+// #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
+// enum Node {
+//     Leaf(Leaf),
+//     Tree(Tree),
+// }
+
+// impl Tree {
+//     pub fn index_of(&self, key: u64) -> u64 {
+//         key
+//     }
+// }
